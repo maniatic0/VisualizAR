5f28e1ce04352be2f4f732366c1f1f3993ac6b12
Christian Oliveros <christianol_01@hotmail.com>
2018-12-18 18:13:45 -0400

    Menu Sound y Colores a botones de menu de vectores

---
+++ Assets/Scripts/Vectores/FunctionPanelColors.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FunctionPanelColors : MonoBehaviour
+{
+    [SerializeField]
+	private UnityEngine.UI.Image suma;
+
+    [SerializeField]
+	private UnityEngine.UI.Image resta;
+
+    [SerializeField]
+	private UnityEngine.UI.Image punto;
+
+    [SerializeField]
+	private UnityEngine.UI.Image cruz;
+
+    [SerializeField]
+	private Color toggleColor = Color.green;
+
+    [SerializeField]
+	private Color inactiveColor = Color.white;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        LimpiarColor();
+    }
+
+    public void LimpiarColor() 
+    {
+        suma.color = inactiveColor;
+        resta.color = inactiveColor;
+        punto.color = inactiveColor;
+        cruz.color = inactiveColor;
+    }
+
+    public void SumaColor() 
+    {
+        suma.color = toggleColor;
+        resta.color = inactiveColor;
+        punto.color = inactiveColor;
+        cruz.color = inactiveColor;
+    }
+
+    public void RestaColor() 
+    {
+        suma.color = inactiveColor;
+        resta.color = toggleColor;
+        punto.color = inactiveColor;
+        cruz.color = inactiveColor;
+    }
+
+    public void PuntoColor() 
+    {
+        suma.color = inactiveColor;
+        resta.color = inactiveColor;
+        punto.color = toggleColor;
+        cruz.color = inactiveColor;
+    }
+
+    public void CruzColor() 
+    {
+        suma.color = inactiveColor;
+        resta.color = inactiveColor;
+        punto.color = inactiveColor;
+        cruz.color = toggleColor;
+    }
+}
