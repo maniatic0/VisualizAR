81f5d392a0af95c8e3823d0ff28781f6705d0f8a
Christian Oliveros <christianol_01@hotmail.com>
2019-01-10 19:55:58 -0400

    Added URL Button for Privacy and AR Targets

---
+++ Assets/Scripts/URLButton.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class URLButton : MonoBehaviour
+{
+    /// <summary>
+    /// Abrir URL especificado usando browser
+    /// </summary>
+    /// <param name="url">URL a abrir</param>
+    public void OpenURL(string url)
+    {
+        Application.OpenURL(url);
+    }
+}
